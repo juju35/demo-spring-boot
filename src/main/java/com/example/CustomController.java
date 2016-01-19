3c2584dda2d6556a401b18431775bec2994f9e43
julien Stéphan <julienstephan29@gmail.com>
2016-01-19 01:47:30 +0100

    init

---
+++ src/main/java/com/example/CustomController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by julien on 19/01/2016.
+ */
+
+@RestController
+public class CustomController {
+
+    @RequestMapping(value = "/custom", method = RequestMethod.POST)
+    public String custom() {
+        return "custom";
+    }
+}
---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .listeners(new ApplicationPidFileWriter())
+                .sources(DemoApplication.class)
+                .run(args);
+
+    }
+}
